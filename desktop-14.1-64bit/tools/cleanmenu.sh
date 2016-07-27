68c1cf4fa377c6a1b074617a1dfb4dba59bb7609
Niki Kovacs <kikinovak@alphamule.microlinux.lan>
2016-07-27 17:58:37 +0200

    ...

--- desktop-14.1-64bit/tools/cleanmenu.sh
+++ desktop-14.1-64bit/tools/cleanmenu.sh
@@ -1,1 +1,21 @@
-../../desktop-14.1-32bit/tools/cleanmenu.sh
+#!/bin/bash
+#
+# cleanmenu.sh
+# (c) Niki Kovacs, 2014
+
+CWD=$(pwd)
+ENTRIESDIR=$CWD/desktop
+ENTRIES=`ls $ENTRIESDIR` 
+MENUDIRS="  /usr/share/applications \
+            /usr/share/distcc \
+            /opt/libreoffice4.2/share/xdg "
+
+for MENUDIR in $MENUDIRS; do
+	for ENTRY in $ENTRIES; do
+		if [ -r $MENUDIR/$ENTRY ]; then
+			echo ":: Updating $ENTRY."
+			cat $ENTRIESDIR/$ENTRY > $MENUDIR/$ENTRY
+		fi
+	done
+done
+
